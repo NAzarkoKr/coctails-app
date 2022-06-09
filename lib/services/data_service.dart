0fcff66529c7cda999de2c2bc0f5dc2c79fe7337
Nazar <nazarkr28@gmail.com>
2022-06-09 21:37:03 +0300

    delete useless

--- lib/services/data_service.dart
+++ lib/services/data_service.dart
@@ -6,20 +6,10 @@ import 'package:http/http.dart' as http;
 class DataService {
   String baseUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php";
   Future<List<CocktailModel>> getList(dynamic page) async {
-    // if (page == 1) {
-    //   page = 'a';
-    // } else if (page == 2) {
-    //   page = 'b';
-    // } else {
-    //   page = 'c';
-    // }
-
     http.Response res = await http.get(Uri.parse(baseUrl + '?f=a'));
 
     List<dynamic> list = json.decode(res.body)['drinks'];
     List<dynamic> newList = [];
-
-    (5 + 5 * page < list.length ? 5 + 5 * page : list.length);
 
     if (page == 1) {
       newList = list.sublist(0, 5);
